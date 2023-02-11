0d08999ba7d6e5ef78051a36a19020b3df56b5d8
2-1-1-2 <fr.tanyan@gmail.com>
2023-02-11 19:49:41 +0100

    mouse & keyboard

--- TP01/sketch.js
+++ TP01/sketch.js
@@ -1,19 +1,23 @@
+
 function setup() {
-  createCanvas(640, 480);
-  print("hello world");
-  frameRate(10)
+  createCanvas(500, 500);
+
+  frameRate(60)
 }
-
 function draw() {
-  if (mouseIsPressed) {
-
-    fill(255);
-    ellipse(mouseX, mouseY, 55, 55);
-  }
-  else {
-    fill(random(255), random(255), random(255));
-  }
-  background(random(255), random(255), random(255), random(50));
-  ellipse(random(640), random(480), 55, 55);
-  line(random(640), random(480), random(640), random(480))
+  let x = mouseX;
+  let y = mouseY;
+  let ix = width - x;
+  let iy = height - y;
+  if (keyIsPressed) background(126)
+  else background(x - y, (x + y) % 255, (x * y) % 255);
+  let colorR = x % 255;
+  let colorG = y % 255;
+  let colorB = (x * y) % 255;
+  if (keyIsPressed) fill(255);
+  else fill(colorR, colorG, colorB);
+  ellipse(x, y, y, y);
+  if (keyIsPressed) fill(0)
+  else fill(255 - colorR, 255 - colorG, 255 - colorB);
+  ellipse(ix, iy, iy, iy);
 }
